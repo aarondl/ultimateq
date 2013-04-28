bb4adc0712f3b0c85910c9a66a6bee7696b065d8
Aaron L <aaron@bettercoder.net>
2013-04-29 00:18:03 +0900

    Adds an error handling test for channel finder.

--- irc/channelfinder_test.go
+++ irc/channelfinder_test.go
@@ -3,9 +3,14 @@ package irc
 import . "launchpad.net/gocheck"
 
 func (s *s) TestChannelFinder(c *C) {
-	finder, err := CreateChannelFinder(`*+?[]()-^`)
+	finder, err := CreateChannelFinder(`~&*+?[]()-^`)
 	c.Assert(err, IsNil)
 	c.Assert(finder.channelRegexp, NotNil)
 	c.Assert(len(finder.FindChannels(")channel")), Equals, 1)
 	c.Assert(finder.IsChannel(")channel"), Equals, true)
+}
+
+func (s *s) TestChannelFinder_Error(c *C) {
+	_, err := CreateChannelFinder(`H`)
+	c.Assert(err, NotNil)
 }
