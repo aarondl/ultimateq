e51775b636b8e55b63bef7c74d207a45ed2ad596
Adam Hamot <Rjhoops92@gmail.com>
2014-06-15 02:02:55 -0400

    Add JSONStorer and tests for it.

---
+++ data/json_storer.go
@@ -0,0 +1,38 @@
+package data
+
+import "encoding/json"
+
+// JSONStorer allows storage of json values into a map.
+type JSONStorer map[string]string
+
+// Put puts a regular string into the map.
+func (js JSONStorer) Put(key, value string) {
+	js[key] = value
+}
+
+// Get gets a regular string from the map.
+func (js JSONStorer) Get(key string) (string, bool) {
+	ret, ok := js[key]
+	return ret, ok
+}
+
+// PutJSON serializes the value and stores it in the map.
+func (js JSONStorer) PutJSON(key string, value interface{}) error {
+	jsMarsh, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	js[key] = string(jsMarsh)
+	return nil
+}
+
+// GetJSON deserializes the value from the map and stores it in intf.
+func (js JSONStorer) GetJSON(key string, intf interface{}) (bool, error) {
+	ret, ok := js[key]
+	if !ok {
+		return false, nil
+	}
+
+	err := json.Unmarshal([]byte(ret), intf)
+	return true, err
+}
---
+++ data/json_storer_test.go
@@ -0,0 +1,32 @@
+package data
+
+import "testing"
+
+func TestJSONStorer(t *testing.T) {
+	js := make(JSONStorer)
+	js.Put("Hello", "world")
+	if got, ok := js.Get("Hello"); !ok || got != "world" {
+		t.Error("Expected world, got", got)
+	}
+
+	st := []struct {
+		Name string
+		Age  int
+	}{
+		{"zamn", 21},
+		{},
+	}
+
+	err := js.PutJSON(st[0].Name, st[0])
+	if err != nil {
+		t.Error("Unexpected marshalling error:", err)
+	}
+
+	if ok, err := js.GetJSON(st[0].Name, &st[1]); !ok {
+		t.Error("Key not found:", ok)
+	} else if err != nil {
+		t.Error("Error unmarshalling value:", err)
+	} else if st[0] != st[1] {
+		t.Errorf("Structs do not match %#v %#v", st[0], st[1])
+	}
+}
