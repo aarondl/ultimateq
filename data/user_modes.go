8b376707d196ef5858b232106334b3d887dfe7c1
Aaron L <aaron@bettercoder.net>
2016-02-20 12:43:09 -0800

    Add JSON for UserModes

--- data/user_modes.go
+++ data/user_modes.go
@@ -2,40 +2,40 @@ package data
 
 // UserModes provides basic modes for users.
 type UserModes struct {
-	modes byte
-	*modeKinds
+	Modes     byte       `json:"modes"`
+	ModeKinds *modeKinds `json:"mode_kinds"`
 }
 
 // NewUserModes creates a new usermodes using the metadata instance for
 // reference information.
 func NewUserModes(m *modeKinds) UserModes {
 	return UserModes{
-		modeKinds: m,
+		ModeKinds: m,
 	}
 }
 
 // SetMode sets the mode given.
 func (u *UserModes) SetMode(mode rune) {
-	u.modes |= u.modeBit(mode)
+	u.Modes |= u.ModeKinds.modeBit(mode)
 }
 
 // HasMode checks if the user has the given mode.
 func (u *UserModes) HasMode(mode rune) bool {
-	bit := u.modeBit(mode)
-	return bit != 0 && (bit == u.modes&bit)
+	bit := u.ModeKinds.modeBit(mode)
+	return bit != 0 && (bit == u.Modes&bit)
 }
 
 // UnsetMode unsets the mode given.
 func (u *UserModes) UnsetMode(mode rune) {
-	u.modes &= ^u.modeBit(mode)
+	u.Modes &= ^u.ModeKinds.modeBit(mode)
 }
 
 // String turns user modes into a string.
 func (u *UserModes) String() string {
 	ret := ""
-	for i := 0; i < len(u.userPrefixes); i++ {
-		if u.HasMode(u.userPrefixes[i][0]) {
-			ret += string(u.userPrefixes[i][0])
+	for i := 0; i < len(u.ModeKinds.userPrefixes); i++ {
+		if u.HasMode(u.ModeKinds.userPrefixes[i][0]) {
+			ret += string(u.ModeKinds.userPrefixes[i][0])
 		}
 	}
 	return ret
@@ -44,9 +44,9 @@ func (u *UserModes) String() string {
 // StringSymbols turns user modes into a string but uses mode chars instead.
 func (u *UserModes) StringSymbols() string {
 	ret := ""
-	for i := 0; i < len(u.userPrefixes); i++ {
-		if u.HasMode(u.userPrefixes[i][0]) {
-			ret += string(u.userPrefixes[i][1])
+	for i := 0; i < len(u.ModeKinds.userPrefixes); i++ {
+		if u.HasMode(u.ModeKinds.userPrefixes[i][0]) {
+			ret += string(u.ModeKinds.userPrefixes[i][1])
 		}
 	}
 	return ret
--- data/user_modes_test.go
+++ data/user_modes_test.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -76,5 +78,34 @@ func TestUserModes_String(t *testing.T) {
 	}
 	if got, exp := m.StringSymbols(), "+"; exp != got {
 		t.Error("Expected: %v, got: %v", exp, got)
+	}
+}
+
+func TestUserModes_JSONify(t *testing.T) {
+	t.Parallel()
+
+	a := NewUserModes(testKinds)
+	a.SetMode('o')
+	var b UserModes
+
+	str, err := json.Marshal(a)
+	if err != nil {
+		t.Error(err)
+	}
+
+	jsonStr := `{"modes":1,"mode_kinds":` +
+		`{"user_prefixes":[["o","@"],["v","+"]],` +
+		`"channel_modes":{"a":1,"b":4,"c":2,"d":3,"x":1,"y":1,"z":1}}}`
+
+	if string(str) != jsonStr {
+		t.Errorf("Wrong JSON: %s", str)
+	}
+
+	if err = json.Unmarshal(str, &b); err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(a, b) {
+		t.Error("A and B differ:", a, b)
 	}
 }
