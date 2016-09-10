c85501625a814b730dcd93b2d7118a78cf420b6d
Aaron L <aaron@bettercoder.net>
2016-09-10 10:41:38 -0700

    Add logout

--- bot/api.go
+++ bot/api.go
@@ -2,19 +2,36 @@ package bot
 
 import (
 	"net"
+	"sync"
 
 	"github.com/aarondl/ultimateq/api"
 	"github.com/aarondl/ultimateq/data"
+	"github.com/aarondl/ultimateq/dispatch/cmd"
+	"github.com/aarondl/ultimateq/irc"
 	"github.com/aarondl/ultimateq/registrar"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
+type msgPipe struct {
+	Events    <-chan *irc.Event
+	CmdEvents <-chan *cmd.Event
+}
+
 // api provides a REST api around a bot
 type apiServer struct {
 	bot   *Bot
 	proxy *registrar.Proxy
+
+	mut   sync.RWMutex
+	pipes map[string]msgPipe
+}
+
+func (a apiServer) HandleRaw(w irc.Writer, ev *irc.Event) {
+}
+
+func (a apiServer) Cmd(command string, w irc.Writer, ev *cmd.Event) {
 }
 
 func newAPIServer(b *Bot) apiServer {
@@ -24,6 +41,26 @@ func newAPIServer(b *Bot) apiServer {
 	}
 
 	return server
+}
+
+func (a apiServer) Pipe(pipe api.Ext_PipeServer) error {
+	return nil
+}
+
+func (a apiServer) Register(ctx context.Context, in *api.RegisterRequest) (*api.Empty, error) {
+	proxy := a.proxy.Get(in.Name)
+	if proxy == nil {
+		return nil, grpc.Errorf(codes.NotFound, "extension not found")
+	}
+
+	ext := in.Name
+	for _, cmd := range in.Cmds {
+	}
+}
+
+func (a apiServer) Unregister(ctx context.Context, in *api.UnregisterRequest) (*api.Empty, error) {
+	a.proxy.Unregister(in.Name)
+	return nil, nil
 }
 
 func (a apiServer) start(port string) error {
@@ -238,11 +275,18 @@ func (a apiServer) StoreUser(ctx context.Context, in *api.Query) (*api.StoredUse
 		return nil, err
 	}
 
+	if user == nil {
+		return nil, grpc.Errorf(codes.NotFound, "user not found")
+	}
+
 	return user.ToProto(), nil
 }
 
 func (a apiServer) StoreUsers(ctx context.Context, _ *api.Empty) (*api.StoredUsersResponse, error) {
-	store := a.bot.Store()
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
 
 	users, err := store.GlobalUsers()
 	if err != nil {
@@ -253,7 +297,10 @@ func (a apiServer) StoreUsers(ctx context.Context, _ *api.Empty) (*api.StoredUse
 }
 
 func (a apiServer) StoreUsersByNetwork(ctx context.Context, in *api.Query) (*api.StoredUsersResponse, error) {
-	store := a.bot.Store()
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
 
 	users, err := store.NetworkUsers(in.Query)
 	if err != nil {
@@ -264,7 +311,10 @@ func (a apiServer) StoreUsersByNetwork(ctx context.Context, in *api.Query) (*api
 }
 
 func (a apiServer) StoreUsersByChannel(ctx context.Context, in *api.NetworkQuery) (*api.StoredUsersResponse, error) {
-	store := a.bot.Store()
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
 
 	users, err := store.ChanUsers(in.Network, in.Query)
 	if err != nil {
@@ -289,259 +339,121 @@ func makeUsersResponse(users []*data.StoredUser) *api.StoredUsersResponse {
 	return &resp
 }
 
-/*
-func (a api) storeChannel(e echo.Context) error {
-	store := a.bot.Store()
-
-	network, err := getParam(e, "network")
-	if err != nil {
-		return err
-	}
-	chanName, err := getParam(e, "channel")
-	if err != nil {
-		return err
-	}
-
-	channel, err := store.FindChannel(network, chanName)
-	if err != nil {
-		return nil
-	}
-	if channel == nil {
-		return echo.NewHTTPError(http.StatusNotFound,
-			fmt.Sprintf("channel %q not found", chanName))
-	}
-
-	e.JSON(http.StatusOK, channel)
-
-	return nil
-}
-
-func (a api) storeChannels(e echo.Context) error {
-	store := a.bot.Store()
-
-	channels, err := store.Channels()
-	if err != nil {
-		return nil
-	}
-
-	e.JSON(http.StatusOK, channels)
-
-	return nil
-}
-
-func (a api) storePutUser(e echo.Context) error {
-	store := a.bot.Store()
-
-	var user data.StoredUser
-	if err := e.Bind(&user); err != nil {
-		return err
-	}
-
-	if err := store.SaveUser(&user); err != nil {
-		return err
-	}
-
-	e.NoContent(http.StatusCreated)
-
-	return nil
-}
-
-func (a api) storePutChannel(e echo.Context) error {
-	store := a.bot.Store()
-
-	var channel data.StoredChannel
-	if err := e.Bind(&channel); err != nil {
-		return err
-	}
-
-	if err := store.SaveChannel(&channel); err != nil {
-		return err
-	}
-
-	e.NoContent(http.StatusCreated)
-
-	return nil
-}
-
-func (a api) storeDeleteUser(e echo.Context) error {
-	store := a.bot.Store()
-
-	username, err := getParam(e, "username")
-	if err != nil {
-		return err
-	}
-
-	ok, err := store.RemoveUser(username)
-	if err != nil {
-		return err
-	}
-
-	if ok {
-		e.NoContent(http.StatusOK)
-	} else {
-		e.NoContent(http.StatusNotFound)
-	}
-
-	return nil
-}
-
-func (a api) storeDeleteChannel(e echo.Context) error {
-	store := a.bot.Store()
-
-	network, err := getParam(e, "network")
-	if err != nil {
-		return err
-	}
-	channel, err := getParam(e, "channel")
-	if err != nil {
-		return err
-	}
-
-	ok, err := store.RemoveChannel(network, channel)
-	if err != nil {
-		return err
-	}
-
-	if ok {
-		e.NoContent(http.StatusOK)
-	} else {
-		e.NoContent(http.StatusNotFound)
-	}
-
-	return nil
-}
-
-func (a api) storeLogout(e echo.Context) error {
-	store := a.bot.Store()
-
-	network, host, username := e.Query("network"), e.Query("host"), e.Query("username")
-	hasNet, hasHost, hasUname := len(network) > 0, len(host) > 0, len(username) > 0
-	if (!hasUname && !hasNet && !hasHost) || (hasUname && (hasNet || hasHost)) {
-		return echo.NewHTTPError(http.StatusBadRequest, "must supply username OR network and host")
-	}
-
-	if hasUname {
-		store.LogoutByUsername(username)
-	} else {
-		store.Logout(network, host)
-	}
-
-	e.NoContent(http.StatusOK)
-
-	return nil
-}
-
-type EchoLogger struct {
-	logger log15.Logger
-}
-
-func (e EchoLogger) Write(b []byte) (int, error) {
-	e.logger.Info(string(b))
-	return len(b), nil
-}
-
-const (
-	bearer = "Bearer"
-)
-
-func jwtAuth(key string) echo.MiddlewareFunc {
-	return func(next echo.Handler) echo.Handler {
-		return echo.HandlerFunc(func(c echo.Context) error {
-			auth := c.Request().Header().Get("Authorization")
-			l := len(bearer)
-			he := echo.ErrUnauthorized
-
-			if len(auth) > l+1 && auth[:l] == bearer {
-				t, err := jwt.Parse(auth[l+1:], func(token *jwt.Token) (interface{}, error) {
-
-					// Always check the signing method
-					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-						return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-					}
-
-					// Return the key for validation
-					return []byte(key), nil
-				})
-				if err == nil && t.Valid {
-					// Store token claims in echo.Context
-					if uqIntf, ok := t.Claims["uq"]; ok {
-						if uq, ok := uqIntf.(string); ok {
-							c.Set("uq", uq)
-						}
-					}
-					if extNameIntf, ok := t.Claims["ext"]; ok {
-						if extName, ok := extNameIntf.(string); ok {
-							c.Set("ext", extName)
-						}
-					}
-
-					return next.Handle(c)
-				}
-			}
-			return he
-		})
-	}
-}
-
-func checkClaims(next echo.Handler) echo.Handler {
-	return echo.HandlerFunc(func(c echo.Context) error {
-
-		var errStr string
-		if uqIntf := c.Get("uq"); uqIntf == nil {
-			errStr = "missing uq claim in token"
-		} else if uq, ok := uqIntf.(string); !ok {
-			errStr = "uq claim in token wrong type"
-		} else if uq != "extension" {
-			errStr = `uq claim in token must be "extension"`
-		}
-
-		if extIntf := c.Get("ext"); extIntf == nil {
-			errStr = `ext claim must exist`
-		} else if ext, ok := extIntf.(string); !ok || len(ext) == 0 {
-			errStr = `ext claim must be a non-empty string`
-		}
-
-		if len(errStr) > 0 {
-			return echo.NewHTTPError(http.StatusBadRequest, errStr)
-		}
-
-		return next.Handle(c)
-	})
-}
-*/
-
-func (a apiServer) Pipe(pipe api.Ext_PipeServer) error {
-	return nil
-}
-
-func (a apiServer) Register(ctx context.Context, in *api.RegisterRequest) (*api.Empty, error) {
-	return nil, nil
-}
-
-func (a apiServer) Unregister(ctx context.Context, in *api.UnregisterRequest) (*api.Empty, error) {
-	return nil, nil
-}
-
 func (a apiServer) StoreChannel(ctx context.Context, in *api.NetworkQuery) (*api.StoredChannel, error) {
-	return nil, nil
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	channel, err := store.FindChannel(in.Network, in.Query)
+	if err != nil {
+		return nil, err
+	}
+
+	if channel == nil {
+		return nil, grpc.Errorf(codes.NotFound, "channel not found")
+	}
+
+	return channel.ToProto(), nil
 }
 
 func (a apiServer) StoreChannels(ctx context.Context, in *api.Empty) (*api.StoredChannelsResponse, error) {
-	return nil, nil
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	channels, err := store.Channels()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(channels) == 0 {
+		return &api.StoredChannelsResponse{}, nil
+	}
+
+	var resp api.StoredChannelsResponse
+	resp.Channels = make([]*api.StoredChannel, len(channels))
+
+	for i, u := range channels {
+		resp.Channels[i] = u.ToProto()
+	}
+
+	return &resp, nil
 }
 
 func (a apiServer) StorePutUser(ctx context.Context, in *api.StoredUser) (*api.Empty, error) {
-	return nil, nil
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	user := new(data.StoredUser)
+	user.FromProto(in)
+
+	return nil, store.SaveUser(user)
 }
 
 func (a apiServer) StorePutChannel(ctx context.Context, in *api.StoredChannel) (*api.Empty, error) {
-	return nil, nil
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	channel := new(data.StoredChannel)
+	channel.FromProto(in)
+
+	return nil, store.SaveChannel(channel)
 }
 
 func (a apiServer) StoreDeleteUser(ctx context.Context, in *api.Query) (*api.Empty, error) {
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	ok, err := store.RemoveUser(in.Query)
+	if err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, grpc.Errorf(codes.NotFound, "user not found")
+	}
+
 	return nil, nil
 }
 
 func (a apiServer) StoreDeleteChannel(ctx context.Context, in *api.NetworkQuery) (*api.Empty, error) {
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	ok, err := store.RemoveChannel(in.Network, in.Query)
+	if err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, grpc.Errorf(codes.NotFound, "user not found")
+	}
+
+	return nil, nil
+}
+
+func (a apiServer) StoreLogout(ctx context.Context, in *api.NetworkQuery) (*api.Empty, error) {
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	store.Logout(in.Network, in.Query)
+	return nil, nil
+}
+
+func (a apiServer) StoreLogoutByUser(context.Context, *Query) (*Empty, error) {
+	store, err := a.getStore()
+	if err != nil {
+		return nil, err
+	}
+
+	store.LogoutByUsername(in.Query)
 	return nil, nil
 }
